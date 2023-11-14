77e0752eb1159cad831527063d5d1a8762510e0a
Ian Wong <ianw35091@gmail.com>
2023-11-14 11:31:19 +0800

    Week5 (2)
    - Changed GameConstants to CameraConstants.
    - Took out all the 'TPC' functions from ThirdPersonCamera.cs and put them into their own classes.

---
+++ Week05/Week05_Student/Assets/Scripts/TPCTopDown.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPCTopDown : TPCBase
+{
+    public TPCTopDown(Transform cameraTransform, Transform playerTransform)
+        : base(cameraTransform, playerTransform)
+    {
+    }
+
+    public override void Update()
+    {
+        // For topdown camera we do not use the x and z offsets.
+        Vector3 targetPos = mPlayerTransform.position;
+        targetPos.y += GameConstants.CameraPositionOffset.y;
+        Vector3 position = Vector3.Lerp(mCameraTransform.position, targetPos, Time.deltaTime * GameConstants.Damping);
+        mCameraTransform.position = position;
+        mCameraTransform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+    }
+}
