16bdc13a6b5d0c919cfa8bc78723fb9e01a1bd8d
Ian Wong <ianw35091@gmail.com>
2023-11-25 17:37:14 +0800

    Update (2)
    Semi-done

--- PGGE-A1-2023/PGGE-A1-2023/Assignment1_UnityProject/Assets/Scripts/TPCBase.cs
+++ PGGE-A1-2023/PGGE-A1-2023/Assignment1_UnityProject/Assets/Scripts/TPCBase.cs
@@ -48,17 +48,29 @@ namespace PGGE
             layerMask = ~layerMask;
 
             RaycastHit hit;
-            
-            //Vector3 playerDistance = mPlayerTransform.position - mCameraTransform.position;
-            //float distanceToObstacle = 0;
+
+            Vector3 newCameraTransform = mCameraTransform.position;
+
+            //Ray cameraRay = new Ray(mCameraTransform.position, mPlayerTransform.position - mCameraTransform.position);
+
+            float playerDistance = Vector3.Distance(mCameraTransform.position * 0.4f, mPlayerTransform.position * 0.1f);
 
             if (Physics.Raycast(mCameraTransform.position, mCameraTransform.TransformDirection(Vector3.forward), 
-                out hit, Mathf.Infinity, layerMask))
+                out hit, playerDistance, layerMask))
             {
+                Debug.DrawRay(mCameraTransform.position, mCameraTransform.TransformDirection(Vector3.forward) * hit.distance, 
+                    Color.yellow);
+
+                newCameraTransform = hit.point;
+                mCameraTransform.position = newCameraTransform;
                 Debug.Log("Hit");
             }
             else
             {
+                Debug.DrawRay(mCameraTransform.position, mCameraTransform.TransformDirection(Vector3.forward) * 100, 
+                    Color.red);
+
+                mCameraTransform.position = newCameraTransform;
                 Debug.Log("No Hit");
             }
         }
